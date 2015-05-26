99e0f9ed26e73f86423cd465b1057af09351b067
belam <belam75@gmail.com>
2015-05-26 15:07:13 -0400

    Added basic layout panes, tested attachpoint inside contentPane

--- src/main/webapp/scripts/quiz/app.js
+++ src/main/webapp/scripts/quiz/app.js
@@ -1,16 +1,45 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/lang",
+    "dijit/layout/BorderContainer",
+    "dijit/layout/ContentPane",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetBase",
     "dojo/text!./templates/App.html"
 ], function(
-    declare, lang, TemplatedMixin, WidgetBase, template
+    declare, lang, BorderContainer, ContentPane, TemplatedMixin, WidgetBase, template
 ){
     return declare("quiz.app", [WidgetBase, TemplatedMixin], {
         
         baseClass : "quiz",
-        templateString : template
+        templateString : template,
+        
+        postCreate : function(){
+            this._buildLayout();
+            this.inherited(arguments);
+        },
+        
+        _buildLayout : function(){
+            this.bc = new BorderContainer({
+                style : "height:100%; width:100%;"
+            }, this.contentNode);
+            this.left = new ContentPane({
+                region : "left",
+                style : "width:30%;"
+            }, this.leftNode);
+            this.center = new ContentPane({
+                region : "center",
+                content : "Center",
+                style : "width:70%;"
+            }, this.centerNode);
+            
+            this.bc.addChild(this.left);
+            this.bc.addChild(this.center);
+            this.foo.innerHTML = "Left by attach point";
+            
+            this.bc.startup();
+            this.inherited(arguments);
+        }
         
     });
 });
